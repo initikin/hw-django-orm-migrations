21ac12da97f61f2d7342826d22482b7e1b7669d1
Ivan Nikitin <asd2211@mail.ru>
2026-03-15 18:04:31 +0800

    initial commit

---
+++ school/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+
+    ordering = 'group'
+
+    students = Student.objects.prefetch_related('teachers').order_by(ordering)
+
+    context = {
+        'object_list': students
+    }
+
+    return render(request, template, context)
